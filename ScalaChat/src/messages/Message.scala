50afa49f0cc71aeeaad512964885f8bb3f9162c7
Stefano <Stefano@DESKTOP-388BCPI>
2017-02-07 16:37:36 +0100

    scala chat

---
+++ ScalaChat/src/messages/Message.scala
@@ -0,0 +1,16 @@
+package messages
+
+import akka.actor.{ActorRef}
+import collection.mutable.Map
+
+sealed trait  Message
+
+case object StartUp extends Message
+case object RegisteredClients extends Message
+
+case class Unregister(identity: String) extends Message
+case class ChatMessage(msg: String) extends Message
+case class PrivateMessage(target: String, msg: String) extends Message
+case class ChatInfo(inf: String) extends Message
+case class RegisterClientMessage(client:ActorRef, identity:String) extends Message
+case class RegisteredClientList(list: Iterable[String]) extends Message
